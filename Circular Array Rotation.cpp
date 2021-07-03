eb95c1c090e5c2719a10766d1538b26b5afc6ab2
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-07-03 20:26:54 +0530

    Add files via upload

---
+++ Circular Array Rotation.cpp
@@ -0,0 +1,23 @@
+int main()
+{
+int n;
+int k;
+int q;
+cin >> n >> k >> q;
+int a[n];
+for (int i=0;i<n;i++)
+ cin >> a[i];
+
+int dest=k%n;
+int b[n];
+for (int i=0;i<n;i++) {
+    b[dest++]=a[i];
+    if (dest==n) dest=0;
+}
+
+for (int i=0;i<q;i++) {
+    int m;
+    cin >> m;
+    cout << b[m] << endl;
+}
+}
