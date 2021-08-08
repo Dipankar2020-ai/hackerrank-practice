6744594f40cca881fa0f19f50d599fcbd008e43b
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-08 08:55:34 -0700

    Create Insert a node at the head of a linked list.cpp

---
+++ Insert a node at the head of a linked list.cpp
@@ -0,0 +1,59 @@
+// Complete the insertNodeAtHead function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtHead(SinglyLinkedListNode* head, int data) {
+  SinglyLinkedListNode* newnode=(SinglyLinkedListNode*)malloc(sizeof(SinglyLinkedListNode)) ;
+  newnode->data=data;
+  newnode->next=NULL;
+  if(head==NULL)
+  {
+      head=newnode;
+  }
+  else 
+  {
+      newnode->next=head;
+      head=newnode;    
+  }
+  return head;
+  
+  
+  
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
+      	SinglyLinkedListNode* llist_head = insertNodeAtHead(llist->head, llist_item);
+        llist->head = llist_head;
+    }
+
+    print_singly_linked_list(llist->head, "\n", fout);
+    fout << "\n";
+
+    free_singly_linked_list(llist->head);
+
+    fout.close();
+
+    return 0;
+}
