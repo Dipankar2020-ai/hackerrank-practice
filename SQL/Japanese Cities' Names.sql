18fb059ee1910b42e5e6a78228b6fc01adb3e837
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-09-25 09:28:31 +0530

    Create Japanese Cities' Names.sql

---
+++ SQL/Japanese Cities' Names.sql
@@ -0,0 +1,5 @@
+Question:->https://www.hackerrank.com/challenges/japanese-cities-name/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+Solution:->
+select NAME
+ from CITY
+ where COUNTRYCODE ='JPN';
