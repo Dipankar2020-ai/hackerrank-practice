14344ccd45cd28db6c731fdded3245ec230767a3
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-09-25 09:25:36 +0530

    Create Japanese Cities' Attributes.sql

---
+++ SQL/Japanese Cities' Attributes.sql
@@ -0,0 +1,7 @@
+Question:->
+https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+Solution:->
+select *
+ from CITY
+ where COUNTRYCODE ='JPN';
